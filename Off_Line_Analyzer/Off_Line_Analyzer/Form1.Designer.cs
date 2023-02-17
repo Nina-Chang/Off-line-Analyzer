9e3057cf3303b8bab59d8760925a36e79f8a53ad
Nina-Chang <85738448+Nina-Chang@users.noreply.github.com>
2023-02-17 15:11:28 +0800

    Add files via upload

---
+++ Off_Line_Analyzer/Off_Line_Analyzer/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Off_Line_Analyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(39, 3);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(133, 47);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox1.Location = new System.Drawing.Point(4, -16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1160, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox2.Location = new System.Drawing.Point(4, 3);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1160, 255);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox3.Location = new System.Drawing.Point(4, 3);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1160, 261);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(211, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Heart Rate(From ECG):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(478, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Temp:";
+            this.label2.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(39, 56);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1031, 202);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(39, 263);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1031, 214);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.Controls.Add(this.pictureBox3);
+            this.panel3.Location = new System.Drawing.Point(39, 485);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1031, 216);
+            this.panel3.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(609, 3);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(131, 48);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(1097, 753);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoad);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Off_Line_Analyzer/Off_Line_Analyzer/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Off_Line_Analyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            rawI = new double[m_a.Length];
+            rawII = new double[m_a.Length];
+            rawV1 = new double[m_a.Length];
+        }
+        StringBuilder res;
+        int iStart, iEnd;
+        List<byte> raw;
+        List<int> Lecg, Lhr, Ltemp;
+        Bitmap bmpI, bmpII, bmpV;
+        int hr, temp;
+        float s0, scaleH;
+        Graphics gI, gII, gV;
+        int ww, hh = 220, iS, iE, x1, y1, dH;
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            gI.Clear(Color.White);
+            gII.Clear(Color.White);
+            gV.Clear(Color.White);
+            label1.Text = "Heart Rate(From ECG):";
+            label2.Text = "Temp:";
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+            string[] strs = File.ReadAllLines(openFileDialog1.FileName);
+            byte bt;
+            foreach (string str in strs)
+            {
+                bt = byte.Parse(str);
+                raw.Add(bt);
+            }
+            decode();
+            displayG();
+        }
+
+        Pen pR, pB, myPen;
+        SolidBrush sbW;
+        int it, ihr, ecg;
+
+        double[] m_a =
+        {
+            0.0024593392459372134, 0.0012694801871167231, -0.0029366492314269123,
+           -0.0058732686125919061, -0.0023466957666592328, 0.0064945535372463231,
+            0.011336261223662733, 0.0035568045681083839, -0.012655166954118943,
+           -0.019960391743576569, -0.0047484627525475782, 0.023622153725863196,
+            0.034690356202778876, 0.0057558397338354468, -0.046622431243507879,
+           -0.067579936172768709, -0.0064297184128108377, 0.13227920610747901,
+            0.27784275491046106, 0.34000000000000002, 0.27784275491046106,
+            0.13227920610747901, -0.0064297184128108377, -0.067579936172768709,
+           -0.046622431243507879, 0.0057558397338354468, 0.034690356202778876,
+            0.023622153725863196, -0.0047484627525475782, -0.019960391743576569,
+           -0.012655166954118943, 0.0035568045681083839, 0.011336261223662733,
+            0.0064945535372463231,-0.0023466957666592328, -0.0058732686125919061,
+           -0.0029366492314269123, 0.0012694801871167231, 0.0024593392459372134
+        };
+    double[] rawI, rawII, rawV1;
+    int[] dtECG = new int[5];
+    Color[] clr = new Color[] { Color.Crimson, Color.PaleVioletRed, Color.LightSlateGray };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnLoad.BackColor = Color.FromArgb(139 , 157, 172);
+            btnClear.BackColor = Color.FromArgb(204, 204, 204);
+            raw = new List<byte>();
+            res = new StringBuilder();
+            dH = 25;
+            pR = new Pen(Color.Red, 2);
+            pB = new Pen(Color.Blue, 2);
+            sbW = new SolidBrush(Color.White);
+        }
+
+        private double FIRfiltering(double[] raw)
+        {
+            int i;
+            double filtered = 0.0;
+            for (i = 0; i < m_a.Length; i++)
+                filtered += m_a[i] * raw[i];
+            return filtered;
+        }
+
+        private void decode()
+        {
+            iStart = 0;
+            iEnd = raw.Count - 14;
+            Lecg = new List<int>();
+            Lhr = new List<int>();
+            Ltemp = new List<int>();
+            int iit = 0, ecgCNT = -1, iihr = 0;
+            while (iStart < iEnd)
+            {
+                if(raw[iStart]==0xFF && raw[iStart + 1] == 0xE2)
+                {
+                    ecgCNT++;
+                    ecg = ((raw[iStart + 2] & 0x7f) << 7) | (raw[iStart + 3] & 0x7f);
+                    if (ecgCNT < m_a.Length - 1)
+                        rawI[m_a.Length - 1 - ecgCNT] = ecg;
+                    else
+                    {
+                        for (int ii = 0; ii < m_a.Length - 1; ii++)
+                            rawI[m_a.Length - 1 - ii] = rawI[m_a.Length - 2 - ii];
+                        rawI[0] = ecg;
+                        ecg = (int)FIRfiltering(rawI);
+                    }
+                    Lecg.Add(ecg);
+                    dtECG[0] = ecg;
+                    //
+                    ecg = ((raw[iStart + 4] & 0x7f) << 7) | (raw[iStart + 5] & 0x7f);
+                    if (ecgCNT < m_a.Length - 1)
+                        rawII[m_a.Length - 1 - ecgCNT] = ecg;
+                    else
+                    {
+                        for (int ii = 0; ii < m_a.Length - 1; ii++)
+                            rawII[m_a.Length - 1 - ii] = rawII[m_a.Length - 2 - ii];
+                        rawII[0] = ecg;
+                        ecg = (int)FIRfiltering(rawII);
+                    }
+                    
+                    dtECG[1] = ecg;
+                    Lecg.Add(ecg);
+                    //
+
+                    ecg = ((raw[iStart + 6] & 0x7f) << 7) | (raw[iStart + 7] & 0x7f);
+                    if (ecgCNT < m_a.Length - 1)
+                        rawV1[m_a.Length - 1 - ecgCNT] = ecg;
+                    else
+                    {
+                        for (int ii = 0; ii < m_a.Length - 1; ii++)
+                            rawV1[m_a.Length - 1 - ii] =rawV1[m_a.Length - 2 - ii];
+                        rawV1[0] = ecg;
+                        ecg = (int)FIRfiltering(rawV1);
+                    }
+      
+                    dtECG[2] = ecg;
+                    Lecg.Add(ecg);
+                    //
+                    hr = ((raw[iStart + 13] & 0x03 << 7) | (raw[iStart + 10] & 0x7F));
+                    dtECG[3] = hr;
+                    if (hr > 0)
+                    {
+                        iihr++;
+                        if(iihr% 256 == 0)
+                        {
+                            ihr++;
+                            ihr %= 3;
+                            //label1.ForeColor = clr[ihr];
+                        }
+                        label1.Text = string.Format("Heart Rate (From ECG): {0} bpm", hr);
+                    }
+                    Lhr.Add(hr);
+                    temp= ((raw[iStart + 13] & 0x0C << 5) | (raw[iStart + 11] & 0x7F));
+                    if(temp>10)
+                    {
+                        label2.Text = string.Format("Temp1:{0:f1} °C", temp / 10.0);
+                        Ltemp.Add(temp);
+                        iit++;
+                        if(iit % 60 == 0)
+                        {
+                            it++;
+                            it %= 3;
+                            label2.ForeColor = clr[it];
+
+                        }
+                    }
+                    temp= ((raw[iStart + 13] & 0x30 << 3) | (raw[iStart + 12] & 0x7F));
+                    if (temp > 10)
+                    {
+                        label2.Text = string.Format("Temp1:{0:f1} °C", temp / 10.0);
+                        Ltemp.Add(temp);
+                        iit++;
+                        if (iit % 60 == 0)
+                        {
+                            it++;
+                            it %= 3;
+                            label2.ForeColor = clr[it];
+
+                        }
+                    }
+                    iStart += 14;
+                }
+                iStart++;
+            }
+        }
+
+        private void displayG()
+        {
+            iS = 0;
+            iE = Lecg.Count / 3;
+            ww = iE;
+            bmpI = new Bitmap(iE, hh);
+            bmpII = new Bitmap(iE, hh);
+            bmpV = new Bitmap(iE, hh);
+            gI = Graphics.FromImage(bmpI);
+            gII = Graphics.FromImage(bmpII);
+            gV = Graphics.FromImage(bmpV);
+            myPen = pB;
+            x1 = 0;
+            int imx, imn, x00 = 0, y00 = 0, x01 = 0, x02 = 0, y01 = 0, y02 = 0;
+            imx = Lecg.Max() + 10;
+            imn = Lecg.Min() - 10;
+            scaleH = (hh - 2.0f * dH) / (imx - imn);
+            iE = Lecg.Count;
+            while (iS < iE - 3)
+            {
+                s0 = Lecg[iS++];
+                y1 = hh - dH - (int)((imx - s0) * scaleH);
+                x1++;
+                gI.DrawLine(myPen, x00, y00, x1, y1);
+                x00 = x1;
+                y00 = y1;
+                s0 = Lecg[iS++];
+                y1=hh-dH- (int)((imx - s0) * scaleH);
+                //gII.DrawLine(myPen, x01*10, y01, x1*10, y1);//波長變長
+                gII.DrawLine(myPen, x01, y01, x1, y1);
+                x01 = x1;
+                y01 = y1;
+                s0 = Lecg[iS++];
+                y1 = hh - dH - (int)((s0 - imn) * scaleH);
+                gV.DrawLine(myPen, x02, y02, x1, y1);
+                x02 = x1;
+                y02 = y1;
+            }
+            pictureBox1.Image = bmpI;
+            pictureBox2.Image = bmpII;
+            pictureBox3.Image = bmpV;
+        }
+    }
+}
